48ece8afaa2f7ef4510c9e65c229ffc5993b9092
Skelda <endlicher.jakub@gmail.com>
2025-02-06 23:37:21 +0100

    fixed some edge cases in bubble

--- Sorting_algorithms/bubble.py
+++ Sorting_algorithms/bubble.py
@@ -1,35 +1,35 @@
 import tkinter as tk
 
 
-def bubbleSortStep(data: list, index: int, number_of_comparisons=0) -> list:
+def bubbleSortStep(
+    data: list, index: int, number_of_comparisons, number_of_swaps
+) -> list:
     """
     Makes exactly one step in the bubble sort.
-    So it either swaps two elements or just increases
-    the index.
+    So it either swaps two elements or does nothing.
     Either way, it increments the number of comparisons by 1.
     """
     number_of_comparisons += 1
     if data[index] > data[index + 1]:
         data[index], data[index + 1] = data[index + 1], data[index]
+        number_of_swaps += 1
 
-    if index < len(data):
-        index += 1
-    else:
-        index = 0
-
-    return number_of_comparisons
+    return number_of_comparisons, number_of_swaps
 
 
-def drawBubble(window, data: list, index, number_of_comparisons) -> None:
+def drawBubble(
+    window, data: list, index, number_of_comparisons, number_of_swaps
+) -> None:
     """
     Draw the partially sorted list after one step of the bubble sort.
     """
     canvas = tk.Canvas(window, width=500, height=400)
     canvas.place(x=0, y=0)
     max_data = max(data)
-    tk.Label(window, text=f"Number of comparisons: {number_of_comparisons}").place(
+    tk.Label(canvas, text=f"Number of comparisons: {number_of_comparisons}").place(
         x=0, y=0
     )
+    tk.Label(canvas, text=f"Number of swaps:       {number_of_swaps}").place(x=0, y=25)
     for i, j in enumerate(data):
         if i == index:
             drawRectangle(canvas, len(data), i, "green", max_data, j)
@@ -39,7 +39,7 @@ def drawBubble(window, data: list, index, number_of_comparisons) -> None:
             drawRectangle(canvas, len(data), i, "blue", max_data, j)
 
     window.update()
-    return bubbleSortStep(data, index, number_of_comparisons)
+    return bubbleSortStep(data, index, number_of_comparisons, number_of_swaps)
 
 
 def drawRectangle(canvas, number_of_rects, position, color, max_data, cur_data):
