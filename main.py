b617e7af81f21de1098aa9c04d3185c9471d6bf9
Skelda <endlicher.jakub@gmail.com>
2025-02-08 08:32:38 +0100

    fixed async functionality

--- main.py
+++ main.py
@@ -37,23 +37,6 @@ def main():
         else:
             run_normal(master, deepcopy(data))
 
-    sorting_running = False
-
-    def stop_sorting():
-        nonlocal sorting_running
-        if sorting_running:
-            sorting_running = False
-            master.quit()
-
-    def start_sorting():
-        nonlocal sorting_running
-        if async_var.get():
-            sorting_running = True
-            run_async(master, deepcopy(data))
-        else:
-            sorting_running = True
-            run_normal(master, deepcopy(data))
-
     async_var = tk.BooleanVar()
     async_var.set(False)
 
@@ -65,9 +48,6 @@ def main():
 
     start_button = tk.Button(switch_frame, text="Start Sorting", command=start_sorting)
     start_button.pack(side=tk.LEFT)
-
-    stop_button = tk.Button(switch_frame, text="Stop Sorting", command=stop_sorting)
-    stop_button.pack(side=tk.LEFT)
     master.mainloop()
 
 
